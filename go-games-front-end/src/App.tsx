71fe40603c1939edb44835ba755400e6928d75b7
gregor-uusvali <gregoruusvali@gmail.com>
2023-09-07 17:28:52 +0300

    starting new api project

---
+++ go-games-front-end/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/tailwind.css'; // Import Tailwind CSS
+
+function App() {
+  return (
+    <div className="bg-yellow-500 text-white p-4">
+      Go play a GAME! 🚀🚀🚀
+    </div>
+  );
+}
+
+export default App;
